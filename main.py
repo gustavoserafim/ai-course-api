c4a3c013fa79f46d0f74a758ccc54a4c8f0e791d
Edgar Gabaldi <edgabaldi@gmail.com>
2024-07-11 18:46:27 -0300

    chore: Update main.py to use lazy loading for images

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from fastapi import BackgroundTasks, FastAPI
 from http.client import HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient
 
-config = dotenv_values(".env")
+from src.content import generate_content
 
+config = dotenv_values(".env")
 
 app = FastAPI()
 
 client = AsyncIOMotorClient(config.get('MONGODB_URI'))
 db = client.yduqs
 
-async def simulate_delay(seconds: int):
-    await asyncio.sleep(seconds)
-    print("Delay complete")
+async def save_generated_content(content: dict):
+    print(">>> Saving content to database")
+    print(f"save_generated_content > content: \n\n{content}")
+    await db.content.insert_one(content)
+    print(">>> Content saved")
 
 @app.get("/")
 async def index():
@@ -26,8 +29,14 @@ async def index():
 @app.post("/generate")
 async def generate(background_tasks: BackgroundTasks):
     try:
-        background_tasks.add_task(simulate_delay, 5)
-        db.content.insert_one({"status": "task started"})
+        kwargs = {
+            "course": "Ciências da Computação",
+            "subject": "Sistemas Operacionais",
+            "topic": "Introdução ao Sistema Operacional",
+            "subtopic": "Origens dos Sistemas Operacionais",
+            "callback": save_generated_content
+        }
+        background_tasks.add_task(generate_content, **kwargs)
         return {
             "status": "task started"
         }
--- src/content.py
+++ src/content.py
@@ -1,41 +1,54 @@
 import json
-import time
-import settings
-import helpers
-import prompts
+import src.helpers as helpers
+import src.prompts as prompts
 from gradio_client import Client
 
-DEBUG = settings.DEBUG
+from dotenv import dotenv_values
 
-client = Client("https://rodrigomasini-mistral-v3.hf.space/")
+config = dotenv_values(".env")
+
+DEBUG = config.get("DEBUG", False)
+LLM_MODEL = config.get('LLM_MODEL')
+
+def initialize_llm_client(model: str):
+    try:
+        client = Client(model)
+        print("LLM client initialized")
+        return client
+    except Exception as e:
+        if DEBUG:
+            print(f"Error initializing client: {e}")
+        return None
+
+client = initialize_llm_client(LLM_MODEL)
 
 def generate_course(course: str, subject: str) -> dict:
-  """
-  This prompt generate a course structure based on 
-  the course and subject.
-  """
-  assert course is not None, "exception:COURSE_NAME_REQUIDED"
-  assert subject is not None, "exception:SUBJECT_NAME_REQUIDED"
+    """
+    This prompt generate a course structure based on 
+    the course and subject.
+    """
+    assert course is not None, "exception:COURSE_NAME_REQUIDED"
+    assert subject is not None, "exception:SUBJECT_NAME_REQUIDED"
 
-  try:
-      prompt = prompts.course_prompt(course, subject)
-      if DEBUG: helpers.input_stats("generate_course", prompt)
+    try:
+        prompt = prompts.course_prompt(course, subject)
+        if DEBUG: helpers.input_stats("generate_course", prompt)
 
-      response = client.predict(
-          message=prompt,
-          history="",
-          temperature=0.1,
-          max_new_tokens=10000,
-          api_name='/predict')
-      
-      if len(response) > 0:
-          print("ENTROU AQUI")
-          if DEBUG: helpers.output_stats("generate_course", response)
-          return json.loads(response)
-      return {}
+        response = client.predict(
+            message=prompt,
+            history="",
+            temperature=0.1,
+            max_new_tokens=10000,
+            api_name='/predict')
+        
+        if len(response) > 0:
+            print("ENTROU AQUI")
+            if DEBUG: helpers.output_stats("generate_course", response)
+            return json.loads(response)
+        return {}
 
-  except Exception as e:
-      print(f"An error occurred: {e}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def generate_outline(
@@ -45,12 +58,12 @@ def generate_outline(
     """
     This prompt generate an outline for a chapter
     """
-  
     assert course is not None, "exception:COURSE_NAME_REQUIDED"
     assert subject is not None, "exception:SUBJECT_NAME_REQUIDED"
     assert chapter is not None, "exception:CHAPTER_NAME_REQUIDED"
 
     try:
+      client = Client(LLM_MODEL)
       prompt = prompts.outline_prompt(course, subject, chapter)
 
       if DEBUG: helpers.input_stats("generate_outline", prompt)
@@ -72,23 +85,22 @@ def generate_outline(
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def generate_content(
-      course: str,
-      subject: str,
-      topic: dict, 
-      subtopic: str,
-      image_quantity: int = 2, 
-      has_video: bool = False):
-    """
-    This prompt generate the content for a subtopic
-    """
+async def generate_content(
+    course: str,
+    subject: str,
+    topic: dict, 
+    subtopic: str,
+    image_quantity: int = 0, 
+    has_video: bool = False,
+    callback=None) -> None:
+
     assert course is not None, "exception:COURSE_NAME_REQUIDED"
     assert subject is not None, "exception:SUBJECT_NAME_REQUIDED"
     assert topic is not None, "exception:TOPIC_NAME_REQUIDED"
     assert subtopic is not None, "exception:SUBTOPIC_NAME_REQUID"
 
-
     try:
+        client = Client(LLM_MODEL)
         prompt = prompts.content_prompt(
             course=course,
             subject=subject,
@@ -103,12 +115,14 @@ def generate_content(
         response = client.predict(
             message=prompt,
             history="",
-            temperature=0.1,
+            temperature=0.2,
             max_new_tokens=10000,
             api_name='/predict')
 
         if len(response) > 0:
             if DEBUG: helpers.output_stats("generate_content", response)
+            if callback:
+                callback(json.loads(response))
             return json.loads(response)
         
         return {}
