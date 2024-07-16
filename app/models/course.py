6ce9013b3da5a8e57fcfc2a6b11a0d954196e427
Edgar Gabaldi <edgabaldi@gmail.com>
2024-07-16 14:18:26 -0300

    feat: @course new fields

--- app/models/course.py
+++ app/models/course.py
@@ -4,13 +4,25 @@ from bson import ObjectId
 from app.schemas.course import CourseResponse
 
 class Course(BaseModel):
-    id: ObjectId = Field(default_factory=ObjectId, alias="_id")
+    id: ObjectId = Field(default_factory=ObjectId, alias="_id", description="Identificador único para o curso")
     name: str
     nature: Optional[str] = None
+    semester_workload: Optional[int] = None
+    weekly_workload: Optional[int] = None
     faculty_profile: Optional[str] = None
+    thematic_area: Optional[str] = None
+    extension_and_research_axis: Optional[str] = None
+    competencies_to_be_developed: Optional[str] = None
     sylabus: Optional[str] = None
     objectives: Optional[str] = None
+    socio_community_objectives: Optional[str] = None
+    description_of_target_audience: Optional[str] = None
+    justification: Optional[str] = None
+    teaching_learning_procedures: Optional[str] = None
     learning_topics: Optional[str] = None
+    assessment_procedures: Optional[str] = None
+    basic_bibliography: Optional[str] = None
+    complementary_bibliography: Optional[str] = None
 
     class Config:
         arbitrary_types_allowed = True
@@ -20,9 +32,21 @@ class Course(BaseModel):
             "id": str(self.id),
             "name": self.name,
             "nature": self.nature,
+            "semester_workload": self.semester_workload,
+            "weekly_workload": self.weekly_workload,
             "faculty_profile": self.faculty_profile,
+            "thematic_area": self.thematic_area,
+            "extension_and_research_axis": self.extension_and_research_axis,
+            "competencies_to_be_developed": self.competencies_to_be_developed,
             "sylabus": self.sylabus,
             "objectives": self.objectives,
-            "learning_topics": self.learning_topics
+            "socio_community_objectives": self.socio_community_objectives,
+            "description_of_target_audience": self.description_of_target_audience,
+            "justification": self.justification,
+            "teaching_learning_procedures": self.teaching_learning_procedures,
+            "learning_topics": self.learning_topics,
+            "assessment_procedures": self.assessment_procedures,
+            "basic_bibliography": self.basic_bibliography,
+            "complementary_bibliography": self.complementary_bibliography,
         }
         return CourseResponse(**data)
--- app/schemas/course.py
+++ app/schemas/course.py
@@ -1,13 +1,25 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CourseBase(BaseModel):
-    name: str
-    nature: Optional[str] = None
-    faculty_profile: Optional[str] = None
-    sylabus: Optional[str] = None
-    objectives: Optional[str] = None
-    learning_topics: Optional[str] = None
+    name: str = Field(..., description="Nome do curso")
+    nature: Optional[str] = Field(None, description="Natureza do curso")
+    semester_workload: Optional[int] = Field(None, description="Carga horária semestral em horas")
+    weekly_workload: Optional[int] = Field(None, description="Carga horária semanal em horas")
+    faculty_profile: Optional[str] = Field(None, description="Perfil do docente responsável pelo curso")
+    thematic_area: Optional[str] = Field(None, description="Área temática do curso")
+    extension_and_research_axis: Optional[str] = Field(None, description="Linha eixo de extensão e pesquisa")
+    competencies_to_be_developed: Optional[str] = Field(None, description="Competências a serem trabalhadas durante o curso")
+    sylabus: Optional[str] = Field(None, description="Ementa do curso")
+    objectives: Optional[str] = Field(None, description="Objetivos do curso")
+    socio_community_objectives: Optional[str] = Field(None, description="Objetivos sociocomunitários do curso")
+    description_of_target_audience: Optional[str] = Field(None, description="Descrição do público envolvido")
+    justification: Optional[str] = Field(None, description="Justificativa para o curso")
+    teaching_learning_procedures: Optional[str] = Field(None, description="Procedimentos de ensino-aprendizagem")
+    learning_topics: Optional[str] = Field(None, description="Temas de aprendizagem abordados no curso")
+    assessment_procedures: Optional[str] = Field(None, description="Procedimentos de avaliação")
+    basic_bibliography: Optional[str] = Field(None, description="Bibliografia básica do curso")
+    complementary_bibliography: Optional[str] = Field(None, description="Bibliografia complementar do curso")
 
 class CourseCreate(CourseBase):
     pass
