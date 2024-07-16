13924e050d38b9e9b9e116b32d16c0aef405ad20
Edgar Gabaldi <edgabaldi@gmail.com>
2024-07-16 11:12:42 -0300

    feat: @courses Add endpoint for generating content for a course and test background task

--- app/api/endpoints/courses.py
+++ app/api/endpoints/courses.py
@@ -1,6 +1,10 @@
+import asyncio
 from typing import List
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
+from app.models.course import Course
+from app.schemas.content import ContentBlock, ContentCreate
 from app.schemas.course import CourseCreate, CourseResponse, CourseUpdate
+from app.services.content_service import ContentService
 from app.services.course_service import CourseService
 
 router = APIRouter()
@@ -26,3 +30,38 @@ async def update_course(
     course: CourseUpdate,
     service: CourseService = Depends()):
     return await service.update_course(course_id, course)
+
+async def task_generate_content(
+    course: Course,
+    content_service: ContentService) -> None:
+
+    await asyncio.sleep(5)
+
+    content = ContentCreate(
+        course_id=str(course.id),
+        name="Generated Content",
+        content=[
+            ContentBlock(
+                type="text", 
+                content="This is a generated content block")
+        ])
+
+    await content_service.create_content(content)
+    print("CONCLUIU A GERAÇÃO")
+
+@router.post("/{course_id}/generate-content", response_model=None)
+async def generate_content(
+    course_id: str, 
+    background_tasks: BackgroundTasks,
+    course_service: CourseService = Depends(),
+    content_service: ContentService = Depends()):
+
+    print("STARTING GENERATION")
+    course = await course_service.get_course(course_id)
+
+    background_tasks.add_task(
+        task_generate_content, 
+        course=course, 
+        content_service=content_service)
+
+    return {"status": "Content generation started"}
