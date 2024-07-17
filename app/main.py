f697f19c761cae5ecc2c15c338612bf1887d04f4
Edgar Gabaldi <edgabaldi@gmail.com>
2024-07-17 08:53:17 -0300

    chore: Add CORS middleware to allow cross-origin requests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,26 @@
 from fastapi import FastAPI
 from app.api.endpoints import router as api_router
 from app.api.endpoints import websocket
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(api_router, prefix="/api")
 app.include_router(websocket.router, tags=["websockets"])
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://your-domain.com",
+    "http://yduqs-poc-dev.us-east-1.elasticbeanstalk.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def index():
